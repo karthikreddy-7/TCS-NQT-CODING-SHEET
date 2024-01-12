7507c53ecb25050a640d5388275a727a3a3ff4cb
karthikreddywiz <karthik.reddy@wizergos.com>
2024-01-12 13:10:26 +0530

    problems on Array

--- Problems_on_Arrays/FindRepeatingElements.java
+++ Problems_on_Arrays/FindRepeatingElements.java
@@ -1,5 +1,27 @@
 package Problems_on_Arrays;
 
+import java.util.*;
+
 public class FindRepeatingElements {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n ];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i: arr){
+            map.put(i,map.getOrDefault(i, 0)+1);
+        }
+        System.out.print("repeating elements in array are ");
+        for (int i: map.keySet()){
+            if (map.get(i)>1){
+                System.out.print(i+" ");
+            }
+        }
+        sc.close();
+    }
     
 }
--- Problems_on_Arrays/MaximumProductSubarray.java
+++ Problems_on_Arrays/MaximumProductSubarray.java
@@ -1,5 +1,27 @@
 package Problems_on_Arrays;
 
+import java.util.Scanner;
+
 public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n ];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for (int i=0;i<n;i++){
+            int tempmax=arr[i];
+            for (int j=i+1;j<n;j++){
+                tempmax*=arr[j];
+                max=Math.max(max,tempmax);
+            }
+            
+        }
+        System.out.println("maximum product subarray is "+max);
+        sc.close();
+    }
     
 }
