5115f80275f3ede80e286f8c5956841a3b7ffccc
subrat yadav <96230177+subrat937@users.noreply.github.com>
2024-04-01 21:59:58 +0530

    Update LargeInArray.java

--- Problems_on_Arrays/LargeInArray.java
+++ Problems_on_Arrays/LargeInArray.java
@@ -12,7 +12,7 @@ public class LargeInArray {
         }
         int max=arr[0];
         for (int i=0;i<n;i++){
-            if (arr[i]<max){
+            if (arr[i]>max){
                 max=arr[i];
             }
         }
