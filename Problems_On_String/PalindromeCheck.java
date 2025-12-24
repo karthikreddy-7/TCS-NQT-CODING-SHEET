052cbf113e6029959f97222a95e314f00705dce0
bharathkumar205 <bharath432005@gmail.com>
2025-12-24 13:15:35 +0530

    Problems_On_String -- PalindromeCheck were updated

--- Problems_On_String/PalindromeCheck.java
+++ Problems_On_String/PalindromeCheck.java
@@ -1,5 +1,32 @@
 package Problems_On_String;
+import java.util.Scanner;
 
 public class PalindromeCheck {
-    
+    public static void main(String[] args)
+    {
+        String inpString , revString = "";
+
+        //Get the Input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String to check for Palindrome :");
+        inpString = sc.nextLine();
+
+        //Reverse the Input string
+        for(int i = inpString.length() -1 ; i >= 0 ; i--)
+        {
+            revString = revString + inpString.charAt(i);
+        }
+
+        //Check if the Input string is equal to the Reversed string
+        if(inpString.equals(revString))
+        {
+            System.out.println("The given String is a Palindrome");
+        }
+        else
+        {
+            System.out.println("The given String is not a Palindrome");
+        }
+
+        sc.close();
+    }
 }
