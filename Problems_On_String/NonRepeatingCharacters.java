fbf644b98b393ca162b0ccabb4fc3bc338934106
Shweta10950 <140787657+Shweta10950@users.noreply.github.com>
2026-03-16 20:28:38 +0530

    Refactor NonRepeatingCharacters class to Main

--- Problems_On_String/NonRepeatingCharacters.java
+++ Problems_On_String/NonRepeatingCharacters.java
@@ -1,5 +1,24 @@
-package Problems_On_String;
+import java.util.*;
 
-public class NonRepeatingCharacters {
-    
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String input = sc.nextLine();
+      
+     Map<Character,Integer> map = new LinkedHashMap<>();
+     for(char ch : input.toCharArray()){
+       if(Character.isLetter(ch)){
+         ch = Character.toLowerCase(ch);
+         map.put(ch, map.getOrDefault(ch,0)+1);
+       }
+       
+     }
+     for(Map.Entry<Character,Integer>entry : map.entrySet()){
+       if(entry.getValue()==1){
+         System.out.println(entry.getKey()+" : "+ entry.getValue());
+       }
+       
+     }
+      sc.close();
+    }
 }
